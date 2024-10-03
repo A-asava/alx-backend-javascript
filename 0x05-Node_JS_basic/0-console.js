4ef048a81280a0c0afcd3285cd8c37c59d42a038
A-asava <adikasaid996@gmail.com>
2024-10-03 09:07:29 +0000

    alx-backend-javascript - Node_JS_basic

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+/**
+ * name of function: displayMessage
+ * prints string name
+*/
+
+function displayMessage(message){
+    console.log(message);
+}
+
+module.exports = displayMessage;
